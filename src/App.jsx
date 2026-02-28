94d249c833012149d9ca4daf491d5a538f7a3445
Suzuki Takafumi <suzukitakafumifcc@gmail.com>
2026-03-01 08:24:59 +0900

    feat: FilterButtonコンポーネント

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Todo from "./components/Todo";
 import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
 
 function App(props) {
   const taskList = props.tasks?.map((task) => (
@@ -16,21 +17,9 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form />
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
       </div>
       <h2 id="list-heading">3 tasks remaining</h2>
       <ul
